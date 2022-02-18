245e7af43f46fd5fa276a780dee5ce0dcbbfcf8e
SkizzerHD <67412692+SkizzerHD@users.noreply.github.com>
2022-02-18 14:24:47 +0100

    adjusted ApplicationLauncher to new JavaFxApplicationLauncher

--- darts_counter_plugin/src/main/java/de/counter/plugin/launch/ApplicationLauncher.java
+++ darts_counter_plugin/src/main/java/de/counter/plugin/launch/ApplicationLauncher.java
@@ -1,29 +1,21 @@
 package de.counter.plugin.launch;
 import org.springframework.context.annotation.ComponentScan;
-
 import de.counter.plugin.language.LanguageSceneController;
 import de.fx.spring.exception.NoSourceClassFoundException;
 import de.fx.spring.exception.NoSourceControllerClassFoundException;
-import de.fx.spring.launch.ApplicationConfiguration;
 import de.fx.spring.launch.JavaFxApplicationLauncher;
-import de.fx.spring.resources.FXRManager;
 
 
 @ComponentScan(basePackages = {"de.counter.plugin.menu.controller",
 "de.counter.plugin.game","de.counter.plugin.x01game.controller",
 "de.counter.plugin.x01game.data","de.counter.plugin.cricket.controller",
 "de.counter.plugin.cricket.data","de.counter.plugin.language","de.counter.plugin.data"})
-public class ApplicationLaunch extends JavaFxApplicationLauncher {
+public class ApplicationLauncher extends JavaFxApplicationLauncher {
+
 	
 	public static void main(String[] args)  {
-		
-		//set the translation-file 
-		FXRManager.setTranslationFile("german_translation.txt");
-		
-		setSourceClass(ApplicationLaunch.class);
-		setSourceController(LanguageSceneController.class);
 		try {
-			launch(ApplicationConfiguration.class,args);
+			launch(ApplicationLauncher.class,LanguageSceneController.class,args);
 		} catch (NoSourceClassFoundException | NoSourceControllerClassFoundException e) {
 			e.printStackTrace();
 		}
