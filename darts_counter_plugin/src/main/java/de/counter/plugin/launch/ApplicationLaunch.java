58be316a885a423352888aed70773b96b074119f
SkizzerHD <67412692+SkizzerHD@users.noreply.github.com>
2022-02-25 13:38:28 +0100

    new ApplicationLauncher class and ApplicationLaunch class main-Method triggers launch-Method from ApplicationLauncher

---
+++ darts_counter_plugin/src/main/java/de/counter/plugin/launch/ApplicationLaunch.java
@@ -0,0 +1,20 @@
+package de.counter.plugin.launch;
+
+/**
+ * 
+ * @author David Baumer
+ *
+ */
+public class ApplicationLaunch {
+	
+	/**
+	 * Only the method launch from ApplicationLauncher which 
+	 * launches the Application gets executed
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ApplicationLauncher.launch(args);
+	}
+	
+
+}
--- darts_counter_plugin/src/main/java/de/counter/plugin/launch/ApplicationLauncher.java
+++ darts_counter_plugin/src/main/java/de/counter/plugin/launch/ApplicationLauncher.java
@@ -6,19 +6,31 @@ import de.fx.spring.exception.NoSourceControllerClassFoundException;
 import de.fx.spring.launch.JavaFxApplicationLauncher;
 
 
+/**
+ * The Class which inherits the JavaFxApplicationLauncher must be the component-scan class.
+ * The method launch triggers the ApplicationConfiguration.launch Method which builds the 
+ * complete Application
+ * 
+ * @author David Baumer
+ * 
+ *
+ */
 @ComponentScan(basePackages = {"de.counter.plugin.menu.controller",
 "de.counter.plugin.game","de.counter.plugin.x01game.controller",
 "de.counter.plugin.x01game.data","de.counter.plugin.cricket.controller",
 "de.counter.plugin.cricket.data","de.counter.plugin.language","de.counter.plugin.data"})
-public class ApplicationLauncher extends JavaFxApplicationLauncher {
-
+public class ApplicationLauncher extends JavaFxApplicationLauncher  {
 	
-	public static void main(String[] args)  {
+	/**
+	 * Application gets builded and launched
+	 * @param args
+	 */
+	public static void launch(String[] args) {
 		try {
-			launch(ApplicationLauncher.class,LanguageSceneController.class,args);
+			launch(ApplicationLauncher.class,LanguageSceneController.class, args);
 		} catch (NoSourceClassFoundException | NoSourceControllerClassFoundException e) {
 			e.printStackTrace();
 		}
 	}
-
+	
 }
